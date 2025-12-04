c60f777e26a5b4ba9f14490ce1832ad7c917ab27
demoutrei-studies <studies.demoutrei@gmail.com>
2025-12-04 17:08:11 +0800

    Added navigation

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const navigation = document.getElementById("navigation");
+
+
+function openNavBar() {
+  navigation.style.display = "block";
+}
+
+
+function closeNavBar() {
+  navigation.style.display = "none";
+}
